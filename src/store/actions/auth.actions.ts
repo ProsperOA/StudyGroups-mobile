735a8a053885e116a785460ca05e730bcc245b8d
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-06-30 19:50:39 -0700

    clean up, validate response before accessing

--- src/store/actions/auth.actions.ts
+++ src/store/actions/auth.actions.ts
@@ -41,12 +41,9 @@ export type AuthAction =
   | ISignUpSuccess
   | ISignUpFailed;
 
-const loginSuccess: ActionCreator<ILoginSuccess> = (data: any): ILoginSuccess => ({
+const loginSuccess: ActionCreator<ILoginSuccess> = (user: any, token: string): ILoginSuccess => ({
   type: types.LOGIN_SUCCESS,
-  payload: {
-    user:  data.user,
-    token: data.auth_token
-  }
+  payload: { user, token }
 });
 
 const loginFailed: ActionCreator<ILoginFailed> = (errMsg: any): ILoginFailed => ({
@@ -54,12 +51,9 @@ const loginFailed: ActionCreator<ILoginFailed> = (errMsg: any): ILoginFailed => 
   payload: errMsg
 });
 
-const signUpSuccess: ActionCreator<ISignUpSuccess> = (data: any): ISignUpSuccess => ({
+const signUpSuccess: ActionCreator<ISignUpSuccess> = (user: any, token: string): ISignUpSuccess => ({
   type: types.SIGNUP_SUCCESS,
-  payload: {
-    user: data.user,
-    token: data.auth_token
-  }
+  payload: { user, token }
 });
 
 const signUpFailed: ActionCreator<ISignUpFailed> = (): ISignUpFailed => ({
@@ -71,24 +65,36 @@ export const authStart: ActionCreator<IAuthStart> = (): IAuthStart => ({
 });
 
 export const login = ({ email, password }: AuthCredentials): any =>
-  (dispatch: Dispatch<AuthAction>): void => {
+  (dispatch: Dispatch<ILoginSuccess | ILoginFailed>): void => {
     axios.post('/login', { email, password })
-      .then(({ data }: AxiosResponse) => dispatch(loginSuccess(data.data)))
-      .catch((err: AxiosError) => dispatch(loginFailed(err.response.data.message)));
+      .then(({ data }: AxiosResponse) => {
+        const { user, auth_token } = data.data;
+
+        storeAuthToken(user.id, auth_token);
+        dispatch(loginSuccess(user, auth_token))
+      })
+      .catch(({ response }: AxiosError) => {
+        dispatch(loginFailed(response ? response.data.message : 'unable to login'))
+      });
 };
 
 export const signUp = (credentials: AuthCredentials): any =>
-  (dispatch: Dispatch<AuthAction>): void => {
-    const { email, password } = credentials;
-
-    const data = {
-      first_name: credentials.firstName,
-      last_name: credentials.lastName,
+  (dispatch: Dispatch<ISignUpSuccess | ISignUpFailed>): void => {
+    const {
+      firstName: first_name,
+      lastName:  last_name,
       email,
       password
-    };
+    } = credentials;
 
-    axios.post('/signup', data)
-      .then(({ data }: AxiosResponse) => dispatch(signUpSuccess(data.data)))
-      .catch(() => dispatch(signUpFailed()));
+    axios.post('/signup', { first_name, last_name, email, password })
+      .then(({ data }: AxiosResponse) => {
+        const { user, auth_token } = data.data;
+
+        storeAuthToken(user.id, auth_token);
+        dispatch(signUpSuccess(user, auth_token));
+      })
+      .catch(({ response }: AxiosError) => {
+        dispatch(signUpFailed(response ? response.data.message : 'unable to sign up'))
+      });
   }
