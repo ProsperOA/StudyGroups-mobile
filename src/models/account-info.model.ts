d4d1807356689b2faa61737181c6681ce34ea43c
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-07-02 01:37:06 -0700

    add account info model

---
+++ src/models/account-info.model.ts
@@ -0,0 +1,9 @@
+export interface AccountInfo {
+  firstName: string;
+  lastName:  string;
+  school:    string;
+  major1:    string;
+  major2:    string;
+  minor:     string;
+  bio:       string;
+}
