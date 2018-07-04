b25677f4eb6ac3a9fed8278acc0d7086ed606de9
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-07-04 00:48:52 -0700

    add: remove auth token

--- src/shared/auth-token.ts
+++ src/shared/auth-token.ts
@@ -1,16 +1,24 @@
 import { AsyncStorage } from 'react-native';
 
+const AUTH_TOKEN_NAME = 'studygroups_auth_token';
+
 export const storeAuthToken = async (userID: string, token: string): Promise<any> => {
   try {
-    await AsyncStorage.setItem('studygroups_auth_token', `${userID}-${token}`);
+    await AsyncStorage.setItem(AUTH_TOKEN_NAME, `${userID}-${token}`);
   }
   catch (error) {
     console.warn(error);
   }
 };
 
-  export const getAuthToken = (callback: (token?: string) => void) => {
-    AsyncStorage.getItem('studygroups_auth_token', (err, authToken) => {
-      callback(authToken || '');
-    });
-  };
+export const getAuthToken = (callback: (token?: string) => void) => {
+  AsyncStorage.getItem(AUTH_TOKEN_NAME, (err, authToken) => {
+    callback(authToken || '');
+  });
+};
+
+export const removeAuthToken = (): void => {
+  AsyncStorage.removeItem(AUTH_TOKEN_NAME, err => {
+    if (err) console.error(err);
+  });
+}
