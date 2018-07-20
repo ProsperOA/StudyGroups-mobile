f91790ae7334d5eed7c3b8585f78bf703e168b2b
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-07-19 20:39:45 -0700

    add study group name

--- src/models/filters/study-groups.filter.ts
+++ src/models/filters/study-groups.filter.ts
@@ -1,6 +1,7 @@
 import { BaseFilter } from './base.filter';
 
 export interface StudyGroupsFilter extends BaseFilter {
+  studyGroupName: string;
   availableSpots: number;
   location:       string;
   courseCode:     string;
